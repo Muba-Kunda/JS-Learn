542ce861aa1a5e95d31c258f2cd57f90e0355cff
Muba-Kunda <69211491+Muba-Kunda@users.noreply.github.com>
2024-06-15 11:18:00 +0000

    comparison

---
+++ JS/JS_basics/04_comparison.js
@@ -0,0 +1,39 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("o2" > 1);
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+// console.log("2" == 02);
+
+// console.log("m" < "n");
+// console.log("m" > "M");
+// console.log("m" == "M");
+
+// console.log("2" != 2);
+// console.log("2" !== 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null != 0);
+// console.log(null === 0);
+// console.log(null !== 0);
+// console.log(Number(null) === 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined != 0);
+// console.log(Number(undefined) == 0);
