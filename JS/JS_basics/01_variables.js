5a574193bee527144f87ffdc78f9b52aaa91dd3d
Muba-Kunda <69211491+Muba-Kunda@users.noreply.github.com>
2024-06-15 06:02:06 +0000

    done config and variables

---
+++ JS/JS_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12113
+var accountEmail = "muba@google.com"
+let accountPassword = "123456"
+accountCity = "Dahod"
+
+accountEmail = "mk@google.com"
+accountPassword = "654321"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
