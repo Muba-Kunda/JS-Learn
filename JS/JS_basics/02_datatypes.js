2064faf49d3c2854d319c4525ecc517fc1e34468
Muba-Kunda <69211491+Muba-Kunda@users.noreply.github.com>
2024-06-15 07:27:43 +0000

    datatype Conversion Operation

---
+++ JS/JS_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+let score = 10
+let string = "muba"
+let loggedIn = true
+let address;
+let temparature = null
+
+
+console.log(typeof score);
+console.log(typeof(string));
+console.log(typeof loggedIn);
+console.log(typeof address);
+console.log(typeof(temparature));
+
+// console.log(score);
+
+/*
+Datatypes:
+
+-primitive
+number (2 power of 53)
+bigint 
+string
+boolean
+undefined
+null
+
+-user defined
+object
+
+*/
---
+++ JS/JS_basics/03_datatypeConversionOpeartion.js
@@ -0,0 +1,69 @@
+let score = "ab"
+
+// console.log(typeof score);
+let scoreInNumber = Number(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+
+let string = null
+
+// console.log(typeof string);
+let inString = String(string)
+// console.log(typeof inString);
+// console.log(inString + "a");
+
+
+
+let num
+
+// console.log(typeof num);
+let inBoolean = Boolean(num)
+// console.log(typeof(inBoolean));
+// console.log(inBoolean);
+
+
+
+
+/* operations */
+let value = 8
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello "
+let str2 = "muba"
+str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 - 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// console.log(2 + 2 * 4 / 2);
+// console.log((2 + 2) * 4 / 2);
+
+//  let num1, num2, num3 = 2 + 2
+//  console.table([num1, num2, num3])
+
+//  let num1 = num2 = num3 = 2 + 2
+//  console.table([num1, num2, num3])
+
+// let gameCounter = 100
+// a = gameCounter++  //post increment
+// console.log(a);
+// b = ++gameCounter  //pre increment
+// console.log(b);
+// console.log(++gameCounter);
+// console.log(gameCounter++);
