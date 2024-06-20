2077b3e28854b8e7f7556311e9e14b6b4519bffc
Muba-Kunda <69211491+Muba-Kunda@users.noreply.github.com>
2024-06-20 06:18:25 +0000

    Number & Math functions

---
+++ JS/JS_basics/07_numbersAndMath.js
@@ -0,0 +1,60 @@
+let score = 100
+// console.log(score);
+
+let num = new Number(300)
+// console.log(num);
+
+// console.log(typeof num);
+// console.log(typeof score);
+
+// console.log(typeof(num.toString()));
+// console.log(typeof(score.toString()));
+
+// console.log(num.toFixed(3));
+
+let anyNum = 3312.4634
+// console.log(anyNum.toPrecision(3));
+
+let hundreds = 10000000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(num.toExponential(0));
+
+// console.log(hundreds.valueOf());
+
+
+// ++++++++++++++++++ Math ++++++++++++++++++++++++++++++++
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.abs(5));
+
+// console.log(Math.round(4.5));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+
+let n = Math.random()
+// console.log(n * 10 + 1);
+// console.log(Math.floor(n * 10 + 1));
+// console.log(Math.floor(n * 10) + 1);
+
+let min = 10
+let max = 20
+
+// console.log(Math.floor((n * (max - min) + 1)) + min);  // by not including min
+// console.log(Math.floor((n * (max - min + 1))) + min);    // by including min 
+
+
+
+
+
+
+
+
+
+
