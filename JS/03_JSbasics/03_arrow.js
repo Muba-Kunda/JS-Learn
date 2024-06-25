1c3a87f084c96f5b115b2665ae1b9bdc65fd05f1
Muba-Kunda <69211491+Muba-Kunda@users.noreply.github.com>
2024-06-25 14:41:45 +0000

    arrow function

---
+++ JS/03_JSbasics/03_arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    userName : "Mubaraka",
+    price : 1000,
+    welcomeMessage : function(){
+        console.log (`${this.userName} , Welcome to website`)
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+user.userName = "Maria"
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+function chai(){
+    let userName = "Moonira"
+    // console.log(this);
+    console.log(this.userName);
+}
+
+// chai()
+
+// const chai2 = function(){
+//     let userName = "Moobaraka"
+//     console.log(this.userName);
+//     console.log(this);
+// }
+
+const chai2 = () => {
+    let userName = "Moobaraka"
+    // console.log(this.userName);
+    console.log(this);
+}
+// chai2()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+
+const addTwo = (num1, num2) => (num1 + num2) 
+console.log(addTwo(6,7)); 
+
+const name = () => ({username: "Mubaraka"})
+console.log(name());
